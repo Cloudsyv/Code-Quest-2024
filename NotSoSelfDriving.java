54526bc9fa1ecd5b6287cb85877ef7067e9bf3cc
Cloudsyv <89221180+Cloudsyv@users.noreply.github.com>
2024-01-10 10:12:35 -0500

    Reverted back to original
    Bruh the site was buggin, original solution worked.

--- NotSoSelfDriving.java
+++ NotSoSelfDriving.java
@@ -5,15 +5,12 @@ class HelloWorld {
         try (Scanner input = new Scanner(System.in)){
             int testCases = Integer.parseInt(input.nextLine());
             for(int testcase = 0; testcase < testCases; testcase++) {
-               
+
                String Information[] =  input.nextLine().split(":");
-               double V = Math.max(0, Math.min(200, Double.parseDouble(Information[0])));
-               double X = Math.max(1, Math.min(400,Double.parseDouble(Information[1])));
-               
-               double Seconds = X/V;
-               if (V != 0 && X != 0) {
-                   Seconds = (Math.round(X*100)/100)/(Math.round(V*100)/100);
-               };
+               double Speed = Double.parseDouble(Information[0]);
+               double Distance = Double.parseDouble(Information[1]);
+
+               double Seconds = Distance/Speed;
                if (Seconds <= 1){
                    System.out.println("SWERVE");
                } else if (Seconds <= 5){
