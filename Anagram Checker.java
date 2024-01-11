f8737aef2dd6fb43464fd4a5b09a572fd39bb7e8
Cloudsyv <89221180+Cloudsyv@users.noreply.github.com>
2024-01-11 02:09:37 -0500

    Update Anagram Checker.java

--- Anagram Checker.java
+++ Anagram Checker.java
@@ -1,6 +1,9 @@
+//Time completed: 30+m
+//Difficulty: Easy
+
 import java.util.Scanner;
 
-class CodeQuest {
+class Anagram {
     public static void main(String[] args) {
         try (Scanner input = new Scanner(System.in)){
             int testCases = Integer.parseInt(input.nextLine());
@@ -12,17 +15,28 @@ class CodeQuest {
                 int Count = 0;
                 // Loop 1st word
                 for (int i = 0; i < Word1.length(); i++){
-                    boolean Stop = false;
                     // Loop through characters of 2nd word
                     for (int v = 0; v < Word2.length(); v++){
-                        if (Word2.substring(v, v+1).equals(Word1.substring(i, i+1)) && Stop == false){
+                        if (Word2.substring(v, v+1).equals(Word1.substring(i, i+1))){
                             Count++;
-                            Stop = true;
+                            break;
                         }
                     }
                 }
                 
-               if (Count == Word1.length() && Word1.equals(Word2) == false){
+                int Count2 = 0;
+                // Loop 2nd word
+                for (int i = 0; i < Word2.length(); i++){
+                    // Loop through characters of 1st word
+                    for (int v = 0; v < Word1.length(); v++){
+                        if (Word1.substring(v, v+1).equals(Word2.substring(i, i+1))){
+                            Count2++;
+                            break;
+                        }
+                    }
+                }
+                
+               if (Count == Word1.length() && Count == Count2 && Word1.equals(Word2) == false && Word1.length() == Word2.length()){
                     System.out.println(Word1+"|"+Word2+" = ANAGRAM");
                 } else {
                   System.out.println(Word1+"|"+Word2+" = NOT AN ANAGRAM");  
