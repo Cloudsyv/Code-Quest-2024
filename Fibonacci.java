ebe83871a690ff49520079aa55c9b399ef113797
Cloudsyv <89221180+Cloudsyv@users.noreply.github.com>
2024-01-24 09:14:36 -0500

    Update Fibonacci.java

--- Fibonacci.java
+++ Fibonacci.java
@@ -9,11 +9,17 @@ class CodeQuest {
                
                long LastPrior[] = {0, 1};
                long Result = 0;
-               for (int i = 0; i < GoalNum-2; i++){
-                   Result = LastPrior[0]+LastPrior[1];
-                   LastPrior[0] = LastPrior[1];
-                   LastPrior[1] = Result;
-               } 
+                if (GoalNum == 1) {
+                    Result = 0;
+                } else if (GoalNum == 2) {
+                    Result = 1;
+                } else {
+                    for (int i = 2; i < GoalNum; i++) {
+                        Result = LastPrior[0] + LastPrior[1];
+                        LastPrior[0] = LastPrior[1];
+                        LastPrior[1] = Result;
+                    }
+                }
                System.out.println(GoalNum+" = "+Result);
             }
         }
