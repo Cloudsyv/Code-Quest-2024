5909ca4d3150a0e8caef2665901fc2125786f97a
Cloudsyv <89221180+Cloudsyv@users.noreply.github.com>
2024-01-29 08:09:38 -0500

    Update leak-loss.java

--- leak-loss.java
+++ leak-loss.java
@@ -1,4 +1,4 @@
-import java.util.Scanner;
+import java.util.*;
 
 class CodeQuest {
     public static void main(String[] args) {
@@ -6,7 +6,8 @@ class CodeQuest {
             int testCases = Integer.parseInt(input.nextLine());
             for(int testcase = 0; testcase < testCases; testcase++) {
                 String Information[] = input.nextLine().split(" ");
-                System.out.println(Integer.parseInt(Information[0])/(Integer.parseInt(Information[1])-Integer.parseInt(Information[2]))*Integer.parseInt(Information[2]));
+                
+                System.out.println(Math.round((double) Integer.parseInt(Information[0])/(Integer.parseInt(Information[1])-Integer.parseInt(Information[2]))*Integer.parseInt(Information[2])));
             }
         }
     }
