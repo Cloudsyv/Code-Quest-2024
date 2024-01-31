afaa84a06f5035a52a124890b76991646c47c943
Cloudsyv <89221180+Cloudsyv@users.noreply.github.com>
2024-01-31 10:10:35 -0500

    Update Morse Code.java

--- Morse Code.java
+++ Morse Code.java
@@ -5,45 +5,40 @@ class CodeQuest {
         try (Scanner input = new Scanner(System.in)){
             int testCases = Integer.parseInt(input.nextLine());
             for(int testcase = 0; testcase < testCases; testcase++) {
-                HashMap<String, String> dict = new HashMap<>();
+                
+                HashMap<String, String> Dict = new HashMap<>();
                 for (int i = 0; i < 26; i++){
-                    String info = input.nextLine();
-                    dict.put(info.substring(0, 1), info.substring(2));
+                  String Info = input.nextLine();
+                  Dict.put(Info.substring(0, 1), Info.substring(2, Info.length())); 
                 }
-
+                
                 // Encode
-                String[] message = input.nextLine().split("");
-                for (int i = 0; i < message.length; i++){
-                    if (message[i].equals(" ")) {
-                        System.out.print("       "); // 7 spaces for word separation
-                        while (i + 1 < message.length && message[i + 1].equals(" ")) {
-                            i++; // Skip extra spaces
-                        }
-                    } else if (dict.containsKey(message[i])){
-                        System.out.print(dict.get(message[i]));
-                        if (i < message.length - 1 && !message[i + 1].equals(" ")) {
-                            System.out.print("   "); // 3 spaces for letter separation
-                        }
+                String Message[] = input.nextLine().split("");
+                for (int i = 0; i < Message.length; i++){
+                  if (Dict.get(Message[i]) == null){
+                    System.out.print("    ");
+                  } else {
+                    System.out.print(Dict.get(Message[i]));
+                    if (i != Message.length-1){
+                      System.out.print("   ");
                     }
+                  }
                 }
-                System.out.println();
-
+                
+                System.out.println("");
+                
                 // Decode
-                String encodedMessage = input.nextLine();
-                String[] encodedWords = encodedMessage.split("       "); // Split by 7 spaces
-                for (String word : encodedWords) {
-                    String[] encodedChars = word.trim().split("   "); // Split by 3 spaces
-                    for (String encodedChar : encodedChars) {
-                        for (String key : dict.keySet()) {
-                            if (dict.get(key).equals(encodedChar)) {
-                                System.out.print(key);
-                                break;
-                            }
-                        }
+                String EncodedMessage[] = input.nextLine().split("   ");
+                for (int i = 0; i < EncodedMessage.length; i++){
+                  for (String v: Dict.keySet()){
+                    if (Dict.get(v).equals(EncodedMessage[i])){
+                      System.out.print(v);
+                    } else if (EncodedMessage[i].length() >= 1 && Dict.get(v).equals(EncodedMessage[i].substring(1, EncodedMessage[i].length()))){
+                      System.out.print(" "+v);
                     }
-                    System.out.print(" ");
+                  }
+                  
                 }
-                System.out.println();
             }
         }
     }
