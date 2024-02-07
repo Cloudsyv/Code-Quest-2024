f2fb979f055933c3de1f13081b6d1a38af956bf2
Cloudsyv <89221180+Cloudsyv@users.noreply.github.com>
2024-02-07 07:37:20 -0500

    Create Change for the world.java

---
+++ Change for the world.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+class CodeQuest {
+    public static void main(String[] args) {
+        try (Scanner input = new Scanner(System.in)){
+            int testCases = Integer.parseInt(input.nextLine());
+            for(int testcase = 0; testcase < testCases; testcase++) {
+                String Info = input.nextLine();
+                double Total = Double.parseDouble(Info.substring(1, Info.length()))*100;
+                int Q = 0;
+                int D = 0;
+                int N = 0;
+                int P = 0;
+                
+                while (Total >= 1){
+                    if (Total >= 25){
+                        Q++;
+                        Total-=25;
+                    } else if (Total >= 10){
+                        D++;
+                        Total-=10;
+                    } else if (Total >= 5){
+                        N++;
+                        Total-=5;
+                    } else if (Total >= 1){
+                        P++;
+                        Total-=1;
+                    }
+                }
+                System.out.println("Quarters="+Q);
+                System.out.println("Dimes="+D);
+                System.out.println("Nickles="+N);
+                System.out.println("Pennies="+P);
+            }
+        }
+    }
+}
